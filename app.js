813775466387b71e4c815c60af170afe6bff0086
Irfan Ezani <irfanezani@gmail.com>
2020-05-22 00:03:01 +0800

    complete functionality

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const button = document.getElementById('joke-btn')
 const jokeContainer = document.querySelector('.joke-container')
+
+button.addEventListener('click',() => {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(response => response.json())
+  .then(data => {
+    change(data)
+  })
+})
+
+
+function change(joke) {
+  jokeContainer.classList.remove('hide') 
+  jokeContainer.innerHTML =
+  `<h3>${joke.value}</h3>`;
+}
